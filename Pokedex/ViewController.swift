4d8027065697842efe5612a45e452f664a4c977c
“Csukás <csukas11@gmail.com>
2022-09-07 17:59:45 +0200

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Takács Ádám on 2022. 09. 07..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
